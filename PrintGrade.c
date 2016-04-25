0351b6586fca7aa21cf6deb664d6664e7d53712a
lunarduan <duan.dao@hotmail.com>
2016-04-25 14:42:22 +0700

    program1 modified

--- PrintGrade.c
+++ PrintGrade.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 void main()
 {
-  char Name[30][40] = {"Sam Matthews", "Mary McBeth", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"}; 
-  int ID[30] = {5301, 5302, 5303, 5304, 5305, 5306}; 
-  float GPA[30] = {2.73, 2.35, 3.65, 3.24, 1.32, 3.26}; 
+  char Name[30][40] = {"Sam Matthews", "Mary McBeth", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"};
+  int ID[30] = {5301, 5302, 5303, 5304, 5305, 5306};
+  float GPA[30] = {2.73, 2.35, 3.65, 3.24, 1.32, 3.26};
   int N = 6, i;
-  int SearchID, found_index; 
- 
-  printf("\nSTUDENT GPA LIST: \n"); 
-  printf("ID----Name------------------------GPA\n"); 
+  int SearchID, found_index;
+
+  printf("\nSTUDENT GPA LIST: \n");
+  printf("ID----Name------------------------GPA\n");
   for (i=0;i<N; i++)
-    printf("%5d %-25s %5.2f\n", ID[i], Name[i], GPA[i]); 
+    printf("%5d %-25s %5.2f\n", ID[i], Name[i], GPA[i]);
   printf("-------------------------------------\n");
-  printf("Enter Student ID to Search (0 to End): "); 
-  scanf("%d", &SearchID); 
+  printf(".....sawasdee user.....\n");
+  printf("Enter Student ID to Search (0 to End): ");
+  scanf("%d", &SearchID);
   while (SearchID)
   {
-      found_index = -1; 
+      found_index = -1;
       for (i = 0; i<N && found_index == -1; i++)
 
         if (SearchID == ID[i])
@@ -25,13 +26,13 @@ void main()
 
       if (found_index == -1)
         printf("Sorry, Student ID %5d not found.\n", SearchID);
-      else 
-        printf("ID: %5d. Name: %-25s. GPA: %5.2f.\n", 
+      else
+        printf("ID: %5d. Name: %-25s. GPA: %5.2f.\n",
            ID[found_index], Name[found_index],  GPA[found_index]);
 
       printf("Enter Student ID to Search (0 to End): ");
 
       scanf("%d", &SearchID);
   }
-  printf("Bye Bye\n"); 
+  printf("Bye Bye\n");
 }
