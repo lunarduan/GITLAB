7248b0c2f67e0d1c5ec0b96fc929211b0be023ae
lunarduan <duan.dao@hotmail.com>
2016-04-25 14:54:26 +0700

    program1 modified

--- PrintGrade.c
+++ PrintGrade.c
@@ -2,9 +2,9 @@
 void main()
 {
   char Name[30][40] = {"Sam Matthews", "Mary McBeth", "Ying Phakpon","Chai Chanchai", "Yong Boonsith", "Kitti Karndee"};
-  int ID[30] = {5301, 5302, 5303, 5304, 5305, 5306};
+  int ID[20] = {5301, 5302, 5303, 5304, 5305, 5306};
   float GPA[30] = {2.73, 2.35, 3.65, 3.24, 1.32, 3.26};
-  int N = 6, i;
+  int N = 10, i;
   int SearchID, found_index;
 
   printf("\nSTUDENT GPA LIST: \n");
